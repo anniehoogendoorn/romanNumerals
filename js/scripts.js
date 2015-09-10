82c3becc7561076dd1e7c139bbf0417d2cdf70a5
Annie Hoogendoorn & Jason Bethel <student@epicodus.com>
2015-09-10 16:46:11 -0700

    rewrite function, pass all tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,44 @@
- var romanNumeral = function(number) {
-   var numbersArray = [1000, 500, 100, 50, 10, 5, 1];
-   var romanArray = ["M", "D", "C", "L", "X", "V", "I"];
+ var romanNumeral = function(input) {
+   var numbersArray = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+   var romanArray = ["M", "CM", "D", "CD", "C", "XC", "L","XL", "X", "IX", "V", "IV", "I"];
+   var number = input;
+   var array = [];
 
-    for (var i=0; i < numbersArray.length; i++ ) {
-      if (numbersArray[i] === number) {
-        return romanArray[i];
-      }
-        else if(number % numbersArray[i] === 0 ) {
-          return romanArray[i].repeat(number);
-
-        } else if(number % numbersArray[i] === 1) {
-          return romanArray[i] + romanArray[6];
-        }
+   while (number > 0) {
+     for (var i=0; i < numbersArray.length; i++ ) {
+       if(number >= numbersArray[i]) {
+         array.push(romanArray[i]);
+         number = number - numbersArray[i];
+       }
+     }
+     i=0;
     }
-          debugger;
- };
+
+    var output = '';
+    for (var i=0; i < array.length; i++) {
+      output += array[i];
+   }
+
+    return output;
+  };
+ //    for (var i=0; i < numbersArray.length; i++ ) {
+ //      if (numbersArray[i] === number) {
+ //        return romanArray[i];
+ //      }
+ //        else if(number % numbersArray[i] === 0 ) {
+ //          return romanArray[i].repeat(number);
+ //
+ //      }
+ //      else if(number % numbersArray[i] === 1) {
+ //          return romanArray[i] + romanArray[6];
+ //      }
+ //      else if(number % numbersArray[i] === 2) {
+ //        return romanArray[i] + romanArray[6].repeat(2);
+ //
+ //      }
+ //      else if(number % numbersArray[i] === 3) {
+ //        return romanArray[i] + romanArray[6].repeat(3);
+ //      }
+ //    }
+ //          debugger;
+ // };
--- spec/specs.js
+++ spec/specs.js
@@ -6,10 +6,20 @@ describe('romanNumeral', function() {
   it("will convert 3 into roman numerals", function() {
     expect(romanNumeral(3)).to.equal('III');
   });
+
   it("will convert 6 into roman numerals", function() {
-    expect(romanNumeral(6)).to.equal("VI");
+    expect(romanNumeral(6)).to.equal('VI');
   });
+
   it("will convert 7 into roman numerals", function() {
-    expect(romanNumeral(7)).to.equal("VII");
+    expect(romanNumeral(7)).to.equal('VII');
   });
+
+  it("will convert 8 into roman numerals", function() {
+    expect(romanNumeral(8)).to.equal('VIII');
+  });
+
+  it("will convert any number to roman numerals", function() {
+    expect(romanNumeral(4)).to.equal('IV');
+  })
 });
