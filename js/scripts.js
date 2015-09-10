e9b52b51206394ca0ca935e01b5ea56cdfc5b2ec
Annie Hoogendoorn & Jason Bethel <student@epicodus.com>
2015-09-10 12:35:07 -0700

    Add scripts function and first spec.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var romanNumeral = function(number) {
+  var romanNumerals = {
+    1: I,
+    5: V,
+    10: X,
+    50: L,
+    100: C,
+    500: D,
+    1000: M
+  }
+
+  if (romanNumerals.indexOf(number) != -1 ) {
+    var output = romanNumerals.number;
+    return output;
+  }
+
+ }
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('romanNumeral', function() {
+  it("will convert numbers into roman numerals by 1", function() {
+    expect(romanNumeral(5)).to.equal('V');
+  });
+});
